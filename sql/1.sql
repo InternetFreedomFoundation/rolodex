4cc81e8d9a984accc35efdbf94d09046c65c84f8
aravind <aravindet@gmail.com>
2017-01-15 01:43:04 +0800

    Initial commit, CLI tool

---
+++ sql/1.sql
@@ -0,0 +1,33 @@
+DROP TABLE IF EXISTS emails;
+DROP TABLE IF EXISTS events;
+
+CREATE TABLE emails (
+	id text PRIMARY KEY,
+	tags text[],
+	data jsonb,
+	done boolean NOT NULL DEFAULT false
+);
+
+CREATE INDEX emails_done
+ON emails(id)
+WHERE done;
+
+CREATE INDEX emails_not_done
+ON emails(id)
+WHERE NOT done;
+
+CREATE INDEX emails_by_tag
+ON emails USING gin(tags);
+
+CREATE TABLE events (
+	id uuid PRIMARY KEY,
+	email text,
+	type text,
+	data jsonb,
+	occurred timestamp,
+	handled timestamp
+);
+
+CREATE INDEX events_email_unprocessed
+ON events(email, occurred)
+WHERE handled IS NULL;
---
+++ token.js
@@ -0,0 +1,75 @@
+/*
+	Rolodex, high-performance email list software.
+
+	Copyright (c) Internet Freedom Foundation.
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+	token.js - Simple Stateless Identity Tokens
+
+	Creates long-lived session tokens for use-cases that preclude
+	server-side session storage. They are similar to JSON Web Tokens
+	but more compact at the cost of flexibility.
+
+	Unlike JWT, they are also not suitable for use cases where the
+	encrypting and decrypting parties are not the same.
+*/
+
+const
+	crypto = require('crypto');
+
+function encode(sub, exp, key) {
+	const
+		msg = Buffer.concat([
+			Buffer.alloc(4, exp.toString(16), 'hex'),
+			Buffer.from(sub)
+		]),
+		hmac = crypto.createHmac('sha224', key).update(msg).digest(),
+		cipher = crypto.createCipher('aes-256-gcm', key),
+		cip = cipher.update(msg, null, 'base64') + cipher.final('base64'),
+		tag = cipher.getAuthTag().toString('base64');
+
+	return cip.replace(/\=+$/, '') + '.' + tag.replace(/\=+$/, '');
+}
+
+function decode(enc, key) {
+	const
+		[ cip, tag ] = enc.split('.'),
+		decipher = crypto.createDecipher('aes-256-gcm', key)
+			.setAuthTag(Buffer.from(tag, 'base64')),
+		msg = Buffer.concat([
+			decipher.update(cip, 'base64'),
+			decipher.final()
+		]);
+
+	return {
+		exp: msg.readUInt32BE(0),
+		sub: msg.toString('utf8', 4)
+	}
+
+}
+
+
+
+//
+// let
+// 	sub = 'thisisalongemailaddresswithmanypieces@gmail.com',
+// 	exp = Math.round(new Date(Date.now() + 30 * 86.4E6).getTime() / 1000),
+// 	key = 'hush, this is a secret',
+// 	enc = encode(sub, exp, key),
+// 	dec = decode(enc, key);
+//
+// console.log(enc.length, enc, dec.sub === sub && dec.exp === exp);
